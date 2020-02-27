2060324151f5e84fac9549a3a79067a967faeef1
newerric <newerric@yahoo.com>
2020-02-27 14:22:57 +0800

    add createButton for UIHlper.swift

--- UIHelper.swift
+++ UIHelper.swift
@@ -10,6 +10,17 @@ import Foundation
 import UIKit
 
 public class UIHelper: NSObject {
+    @objc static func createButton(title: String,target: AnyObject, selector: Selector) -> UIButton {
+        let button = UIButton()
+        button.backgroundColor = .lightGray
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 22.0)
+        button.addTarget(target, action: selector, for: .touchUpInside)
+        
+        return button
+    }
+    
     @objc static func createButton(title: String,target: AnyObject, selector: Selector, frame: CGRect) -> UIButton {
         let button = UIButton(frame: frame)
         button.backgroundColor = .lightGray
