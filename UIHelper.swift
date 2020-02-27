bb33868786ff9daf24c0de3509c30c154fc112f0
newerric <newerric@yahoo.com>
2020-02-27 13:36:06 +0800

    add UIHelper.swift

---
+++ UIHelper.swift
@@ -0,0 +1,23 @@
+//
+//  UIHelper.swift
+//  Swift Helpers
+//
+//  Created by mikewang on 2020/2/15.
+//  Copyright © 2020 mikewang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class UIHelper: NSObject {
+    @objc static func createButton(title: String,target: AnyObject, selector: Selector, frame: CGRect) -> UIButton {
+        let button = UIButton(frame: frame)
+        button.backgroundColor = .lightGray
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 22.0)
+        button.addTarget(target, action: selector, for: .touchUpInside)
+        
+        return button
+    }
+}
